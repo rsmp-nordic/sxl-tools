8169f16e2ec877e919fc294b328c072cc5150bed
David Otterdahl <david.otterdahl@gmail.com>
2013-11-27 16:08:59 +0100

    makesxl.pl: Add proper options

--- makesxl.pl
+++ makesxl.pl
@@ -1,9 +1,6 @@
 #!perl
-#!/usr/bin/perl -w
 # Exports signal exchange lists (SXL) in Excel-format to CSV files
 # Requires ActiveState Perl, Win32::OLE and MS Excel
-# TODO: Requires "Objects" directory
-# TODO: Doesn't quit Excel
 
 # Copyright (C) 2012,2013 David Otterdahl <david.otterdahl@gmail.com>
 
@@ -11,30 +8,45 @@ use Win32::OLE qw(EVENTS);
 use Win32::OLE::Const 'Microsoft Excel';
 use Win32::OLE::Variant;
 use Win32::OLE::NLS qw(:LOCALE :DATE);
+use Getopt::Long;
 
 $Win32::OLE::Warn = 3; # Die on Errors.
 
-# Change $obj_path to something useful e.g.:
-# 'C:\\Documents and Settings\\User\\Desktop\\'
-my $obj_path = 'C:\\tmp\\';
-my $excelfile = 'SXL.xlsx';
+my $obj_path;
+my $excelfile;
+my $visible;
+GetOptions(
+	"path=s" => \$obj_path,
+	"sxl=s" => \$excelfile,
+	"visible" => \$visible,
+	);
+
+unless (defined($obj_path) and defined($excelfile)) {
+	print "usage: makesxl.sl --path [path] --sxl [sxl-file] [--visible]";
+	exit 1;
+}
 
 my $Excel = Win32::OLE->GetActiveObject('Excel.Application')
 	|| Win32::OLE->new('Excel.Application', 'Quit');
 
 $Excel->{DisplayAlerts}=0;
 
-my $Book = $Excel->Workbooks->Open($obj_path.$excelfile);
+my $Book = $Excel->Workbooks->Open($obj_path."\\".$excelfile);
 
 # Make Excel visible during export (disabled by default)
-#$Excel->{Visible} = 1;
+if (defined($visible)) {
+	$Excel->{Visible} = 1;
+}
+
+system "mkdir \"$obj_path\\Objects\"";
 
 my $sheetcnt = $Book->Worksheets->Count();
 foreach (1..$sheetcnt) {
 	my $name = $Book->Worksheets($_)->{Name};
 	$name =~ s/\ /_/g;
+	$name = "$obj_path\\Objects\\$name.csv";
 	$Book->Worksheets($_)->Activate();
-	$Book->SaveAs($obj_path.'Objects\\'.$name.'.csv', xlCSV);
+	$Book->SaveAs($name, xlCSV);
 }
 
 undef $Book;
