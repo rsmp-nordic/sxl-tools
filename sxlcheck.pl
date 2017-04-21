abe82bfe903e3ecbdd730732787ced4c23b5cb5b
David Otterdahl <david.otterdahl@gmail.com>
2017-04-21 16:57:36 +0200

    sxlcheck.pl: Print the entire content of the SXL in markdown

--- sxlcheck.pl
+++ sxlcheck.pl
@@ -5,13 +5,12 @@
 
 # TODO: Test for spaces before or after
 # TODO: Test for incorrect cId matches between single and grouped objects
+# TODO: Sheet 'Object types'
+# TODO: Sheet 'Aggregated status'
 
 use strict;
 use Spreadsheet::XLSX;
 use Getopt::Long;
-
-my $all; # Print all details
-GetOptions("all" => \$all);
 
 my @files = @ARGV;
 my $fname;
@@ -26,137 +25,163 @@ sub read_sxl {
 	my $workbook = Spreadsheet::XLSX->new($fname);
 	foreach $sheet (@{$workbook->{Worksheet}}) {
 		if($sheet->{Name} eq "Version") {
-			cprint($sheet, 3, 1, "Plant Id:      ");
-			cprint($sheet, 5, 1, "Plant Name:    ");
-			cprint($sheet, 9, 1, "Constructor:   ") if defined($all);
-			cprint($sheet, 11,1, "Reviewed:      ") if defined($all);
-			cprint($sheet, 14,1, "Approved:      ") if defined($all);
-			cprint($sheet, 17,1, "Created date:  ");
-			cprint($sheet, 20,1, "SXL revision:  ");
-			cprint($sheet, 20,2, "Revision date: ");
-			cprint($sheet, 25,1, "RSMP version:  ") if defined($all);
-		} elsif($sheet->{Name} eq "Object types") {;
-		} elsif($sheet->{Name} eq "Aggregated status") {;
-		} elsif($sheet->{Name} eq "Alarms") {;
-			my $y = 6;
-			while (test($sheet, $y, 7)) {
-				my $x = 0;
-				# status
-				cprint($sheet, $y, $x++, "ObjectType: ") if defined($all);
-				cprint($sheet, $y, $x++, "Object (optional): ") if defined($all);
-				cprint($sheet, $y, $x++, "alarmCodeId: ") if defined($all);
-				cprint($sheet, $y, $x++, "Description: ") if defined($all);
-				cprint($sheet, $y, $x++, "externalAlarmCodeId: ") if defined($all);
-				cprint($sheet, $y, $x++, "externalNtsAlarmCodeId: ") if defined($all);
-				cprint($sheet, $y, $x++, "Priority: ") if defined($all);
-				cprint($sheet, $y, $x++, "Category: ") if defined($all);
-
-				# return value
-				while(test($sheet, $y, $x)) {
-					cprint($sheet, $y, $x++, "Name: ") if defined($all);
-					cprint($sheet, $y, $x++, "Type: ") if defined($all);
-					cprint($sheet, $y, $x++, "Value: ") if defined($all);
-
-					# Check for semicolon in the "Comment" field
-					semi_check($sheet, $x, $y);
-
-					cprint($sheet, $y, $x++, "Comment: ") if defined($all);
-
-				}
-				$y++;
-			}
-		} elsif($sheet->{Name} eq "Status") {;
-			my $y = 6;
-			while (test($sheet, $y, 7)) {
-				my $x = 0;
-				# status
-				cprint($sheet, $y, $x++, "ObjectType: ") if defined($all);
-				cprint($sheet, $y, $x++, "Object (optional): ") if defined($all);
-				cprint($sheet, $y, $x++, "statusCodeId: ") if defined($all);
-				cprint($sheet, $y, $x++, "Description: ") if defined($all);
-
-				# return value
-				while(test($sheet, $y, $x)) {
-					cprint($sheet, $y, $x++, "Name: ") if defined($all);
-					cprint($sheet, $y, $x++, "Type: ") if defined($all);
-					cprint($sheet, $y, $x++, "Value: ") if defined($all);
-
-					# Check for semicolon in the "Comment" field
-					semi_check($sheet, $x, $y);
-
-					cprint($sheet, $y, $x++, "Comment: ") if defined($all);
-
-				}
-				$y++;
-			}
-		} elsif($sheet->{Name} eq "Commands") {;
-
-			# Sanity check. Check for semicolon in the "Comment" field for the first
-			# argument in all command sections
-			# TODO: Check in all the arguments values; not just the first one
-			my $sec;
-			my $y;
-			my @sections = command_section($sheet);
-			foreach $sec (@sections) {
-				# Need to check each command section
-				$y = $sec;
-				while (test($sheet, $y, 8)) {
-					my $x = 0;
-					# commands
-					cprint($sheet, $y, $x++, "ObjectType: ") if defined($all);
-					cprint($sheet, $y, $x++, "Object (optional): ") if defined($all);
-					cprint($sheet, $y, $x++, "commandCodeId: ") if defined($all);
-					cprint($sheet, $y, $x++, "Description: ") if defined($all);
-
-					# argument
-					while(test($sheet, $y, $x)) {
-						cprint($sheet, $y, $x++, "Name: ") if defined($all);
-						cprint($sheet, $y, $x++, "Command: ") if defined($all);
-						cprint($sheet, $y, $x++, "Type: ") if defined($all);
-						cprint($sheet, $y, $x++, "Value: ") if defined($all);
-	
-						# Check for semicolon in the "Comment" field
-						semi_check($sheet, $x, $y);
-	
-						cprint($sheet, $y, $x++, "Comment: ") if defined($all);
-	
-					}
-
-					$y++;
-				}
-			}
-		} else {
-			# Object sheet
-			printf "Sheet: ".$sheet->{Name}."\n" if defined($all);
-			cprint($sheet, 1,1, "SiteId:        ");
-			cprint($sheet, 1,2, "Description:   ");
-
-			# Print all grouped objects
-			printf "Grouped objects\n";
-			my $y = 6;
-			while (test($sheet, $y, 0)) {
-				if (defined($all)) {
-					oprint($sheet, $y);
-				}
-				$y++;
-			}
-
-			# if not "all". Just print the last grouped object
-			unless (defined($all)) {
-				oprint($sheet, $y-1);
-			}
-
-			# Print all single objects
-			printf "Single objects\n";
-			$y = 24;
-			while (test($sheet, $y, 0)) {
-				oprint($sheet, $y) if defined($all);
-				$y++;
-			}
-
-			# if not "all". Just print the last single object
-			oprint($sheet, $y-1) unless (defined($all));
+			print_version($sheet);
+		} elsif($sheet->{Name} eq "Object types") {
+			# STUB
+		} elsif($sheet->{Name} eq "Aggregated status") {
+			# STUB
+		} elsif($sheet->{Name} eq "Alarms") {
+			print_alarms($sheet);
+		} elsif($sheet->{Name} eq "Status") {
+			print_status($sheet);
+		} elsif($sheet->{Name} eq "Commands") {
+			print_commands($sheet);
+		} else { # Objects
+			print_objects($sheet);
 		}
+	}
+}
+
+sub print_version {
+	my $sheet = shift;
+	printf("# Signal Exchange List\n");
+
+	cprint($sheet, 3, 1, "Plant Id");
+	cprint($sheet, 5, 1, "Plant Name");
+	cprint($sheet, 9, 1, "Constructor");
+	cprint($sheet, 11,1, "Reviewed");
+	cprint($sheet, 14,1, "Approved");
+	cprint($sheet, 17,1, "Created date");
+	cprint($sheet, 20,1, "SXL revision");
+	cprint($sheet, 20,2, "Revision date");
+	cprint($sheet, 25,1, "RSMP version");
+}
+
+sub print_alarms {
+	my $sheet = shift;
+	printf("\n# Alarms\n");
+	
+	my $noReturnValues = get_no_return_values($sheet, 6);
+
+	# Print header
+	my $i;
+	printf "| ObjectType | Object (optional) | alarmCodeId | Description | externalAlarmCodeId | externalNtsAlarmCodeId | Priority | Category |";
+	for($i=0; $i<$noReturnValues; $i++) {
+		printf "|Name|Type|Value|Comment|";
+	}
+	print "\n";
+	printf "| ---------- | ----------------- |:-----------:| ----------- | ------------------- | ---------------------- |:--------:|:--------:|";
+	for($i=0; $i<$noReturnValues; $i++) {
+		printf "|----|----|-----|-------|";
+	}
+	print "\n";
+
+	# Print alarms
+	my $y = 6;
+	while (test($sheet, $y, 7)) {
+		aprint($sheet, $y, 8, 4, 2);
+		$y++;
+	}
+}
+
+sub print_status {
+	my $sheet = shift;
+	printf("\n# Status\n");
+
+	my $noReturnValues = get_no_return_values($sheet, 6);
+
+	# Print header
+	my $i;
+	printf "| ObjectType | Object (optional) | statusCodeId | Description |";
+	for($i=0; $i<$noReturnValues; $i++) {
+		printf "|Name|Type|Value|Comment|";
+	}
+	print "\n";
+	printf "| ---------- | ----------------- |:-----------:| ----------- |";
+	for($i=0; $i<$noReturnValues; $i++) {
+		printf "|----|----|-----|-------|";
+	}
+	print "\n";
+	
+	# Print status
+	my $y = 6;
+	while (test($sheet, $y, 7)) {
+		aprint($sheet, $y, 4, 4, 2);
+		$y++;
+	}
+}
+
+sub print_commands {
+	my $sheet = shift;
+	printf("\n# Commands\n");
+
+	my $sec;
+	my $y;
+	my @sections = command_section($sheet);
+
+	# Find max number of return values in each section
+	my $noReturnValues = 0;
+	foreach $sec (@sections) {
+		$y = $sec;
+		if(get_no_return_values($sheet, $y) > $noReturnValues) {
+			$noReturnValues = get_no_return_values($sheet, $y);
+		}
+	}
+
+	# Print header
+	my $i;
+	printf "| ObjectType | Object (optional) | commandCodeId | Description |";
+	for($i=0; $i<$noReturnValues; $i++) {
+		printf "|Name|Command|Type|Value|Comment|";
+	}
+	print "\n";
+	printf "| ---------- | ----------------- |:-----------:| ----------- |";
+	for($i=0; $i<$noReturnValues; $i++) {
+		printf "|----|----|----|------|-------|";
+	}
+	print "\n";
+
+	foreach $sec (@sections) {
+		# Need to check each command section
+		$y = $sec;
+
+		# Print command
+		while (test($sheet, $y, 7)) {
+			aprint($sheet, $y, 4, 5, 3);
+			$y++;
+		}
+	}
+	print "\n";
+}
+
+sub print_objects {
+	my $sheet = shift;
+	# Object sheet
+	printf("\nObjects\n");
+	printf("=======\n");
+	cprint($sheet, 1,1, "SiteId:");
+	cprint($sheet, 1,2, "Description:");
+
+	# Print all grouped objects
+	printf "\nGrouped objects\n";
+	printf "---------------\n";
+	printf "|ObjectType|Object|componentId|NTSObjectId|externalNtsId|Description|\n";
+	printf "|----------|------|-----------|-----------|-------------|-----------|\n";
+	my $y = 6;
+	while (test($sheet, $y, 0)) {
+		oprint($sheet, $y);
+		$y++;
+	}
+
+	# Print all single objects
+	printf "\nSingle objects\n";
+	printf "--------------\n";
+	printf "|ObjectType|Object|componentId|NTSObjectId|externalNtsId|Description|\n";
+	printf "|----------|------|-----------|-----------|-------------|-----------|\n";
+	$y = 24;
+	while (test($sheet, $y, 0)) {
+		oprint($sheet, $y);
+		$y++;
 	}
 }
 
@@ -167,15 +192,16 @@ sub cprint {
 	my $x = shift;
 	my $text = shift;
 	my $val = $sheet->{Cells}[$y][$x]->{Val};
-	if (defined($val)) {
-	        $val =~ s/%/%%/g;
-		printf $text.$val."\n";
-	} else {
-		printf "$text(not defined)\n";
+
+	unless(defined($val)) {
+		# Warning: value not defined"
+		$val = " ";
 	}
+        $val =~ s/%/%%/g; # Needed for printf()
+	printf "**$text**: $val  \n";
 }
 
-# Object print
+# Print object
 sub oprint {
 	my $sheet = shift;
 	my $y = shift;
@@ -187,10 +213,81 @@ sub oprint {
 	my $externalNtsId = $sheet->{Cells}[$y][4]->{Val};
 
 	unless (defined($object) and defined($cId) and defined($ntsoId)) {
-		print "WARNING: row $y incomplete\n";
+		print STDERR "WARNING: row $y incomplete\n";
 	} else {
-		printf "$object $cId $ntsoId\n";
+		printf "|$object|$cId|$ntsoId|\n";
 	}
+}
+
+# Print alarm/status/commands
+sub aprint {
+	my $sheet = shift;
+	my $y = shift;  # Start row
+	my $col_length = shift; # 8 for alarms, 4 for status and commands
+	my $return_value_col_length = shift; # 4 for alarm and status, 5 for commands
+	my $value_list_col = shift; # this column of return values/arguments should be split into bullet list, 2 for alarm and status, 3 for commands
+
+	# Get values for a row
+	my $i;
+	my $x = 0;
+	my @val;
+	for($i = 0; $i < $col_length; $i++) {
+		$val[$i] = $sheet->{Cells}[$y][$x++]->{Val};
+
+		unless(defined($val[$i])) {
+			$val[$i] = "";
+		}
+	}
+
+	# Print row
+	print "|";
+	for($i = 0; $i < $col_length; $i++) {
+		$val[$i] =~ s/\r//g;
+		$val[$i] =~ s/\n/<br>/g;
+		print "$val[$i]|";
+	}
+
+	# return values
+	while(test($sheet, $y, $x)) {
+		# Get values for a row
+		$col_length = $return_value_col_length;
+		for($i = 0; $i < $col_length; $i++) {
+			$val[$i] = $sheet->{Cells}[$y][$x++]->{Val};
+			unless(defined($val[$i])) {
+				$val[$i] = "";
+			}
+		}
+
+		# Check for semicolon in the "Comment" field, which is the last one
+		semi_check($sheet, $x, $y);
+
+		# Print row
+		print "|";
+		for($i = 0; $i < $col_length; $i++) {
+			# 'Value' should be split into bullet list (HTML)
+			$val[$i] =~ s/-//g;	# Remove '-'
+			if($i == $value_list_col) {
+				# Find line breaks and convert to them to bullet list in HTML
+				if($val[$i] =~ /\r\n/) {
+					$val[$i] =~ s/\r//g;
+					my @list = split("\n", $val[$i]);
+					my $v;
+					$val[$i] = "<ul>";
+					foreach $v (@list) {
+						$val[$i] = $val[$i]."<li>$v</li>";
+					}
+					$val[$i] = $val[$i]."</ul>";
+				}
+			}
+			else {
+				# Remove line breaks
+				$val[$i] =~ s/\r//g;
+				$val[$i] =~ s/\n/<br>/g;
+			}
+			print "$val[$i]|";
+		}
+	}
+	print "\n";
 }
 
 # Find command section
@@ -228,8 +325,9 @@ sub semi_check {
 	my $x = shift;
 	my $y = shift;
 	my $comment = $sheet->{Cells}[$y][$x]->{Val};
-
-	printf "WARNING: Found semicolon in comment field: $comment\n" if ($comment =~ /;/);
+	if(defined($comment)) {
+		printf STDERR "WARNING: Found semicolon in comment field: $comment\n" if ($comment =~ /;/);
+	}
 }
 
 # Test for contens if the first column
@@ -238,4 +336,21 @@ sub test {
 	my $y = shift;
 	my $x = shift;
 	return defined($sheet->{Cells}[$y][$x]->{Val});
+}
+
+# Get max number of arguments/return values
+sub get_no_return_values {
+	my $sheet = shift;
+	my $y = shift; # start row, alarms, status: 6, commands: variable
+
+	my $noReturnValues = 0;
+	my $x = 8; # first return value
+	while (test($sheet, $y, 7)) {
+		while(test($sheet, $y, $x)) {
+			$noReturnValues++;
+			$x += 4;
+		}
+		$y++
+	}
+	return $noReturnValues;
 }
