0bdd4aa54d25a6f5431a7aad2740e2468611d2ed
David Otterdahl <david.otterdahl@gmail.com>
2021-09-27 14:04:04 +0200

    xlsx2csv: Print to CSV files rather than STDOUT

--- xlsx2csv.rb
+++ xlsx2csv.rb
@@ -4,21 +4,23 @@ require 'rubyXL/convenience_methods'
 
 # xlsx2csv reads sxl i excel format and outputs to csv-format
 
-XLSX = ARGV[0]
-workbook = RubyXL::Parser.parse(XLSX)
+xlsx = ARGV[0].dup
+workbook = RubyXL::Parser.parse(xlsx)
 
 system("mkdir Objects")
 workbook.each do |sheet|
-  row = 0
-  while sheet[row] != nil do
-    line = ""
-    col = 0
-    while sheet[row][col] != nil do
-      line = line + sheet[row][col].value.to_s + ";" if sheet[row][col].value != nil
-      col = col + 1
+  File.open("Objects/" + sheet.sheet_name + ".csv", "w") do |f|
+    row = 0
+    while sheet[row] != nil do
+      line = ""
+      col = 0
+      while sheet[row][col] != nil do
+        line = line + sheet[row][col].value.to_s + ";" if sheet[row][col].value != nil
+        col = col + 1
+      end
+      f.write line
+      row = row + 1
     end
-    puts line
-    row = row + 1
   end
 end
 
@@ -26,6 +28,6 @@ system("zip -q -j Objects.zip Objects/*")
 system("rm -rf Objects")
 
 # Rename after source file
-XLSX.gsub!(/ /, '_')
-XLSX.gsub!(/.xlsx/, '')
-system("mv Objects.zip " + XLSX + ".zip")
+xlsx.gsub!(/ /, '_')
+xlsx.gsub!(/.xlsx/, '')
+system("mv Objects.zip " + xlsx + ".zip")
